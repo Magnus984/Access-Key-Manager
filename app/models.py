adb721077f67240e58130ad40a0b7a1e554a749c
Magnus <nharnharyhaw984@gmail.com>
2024-06-25 01:16:26 +0000

    changed the character length of field

--- app/models.py
+++ app/models.py
@@ -24,7 +24,7 @@ class IT_Personnel(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
-    school_email = models.CharField(max_length=30, default='school_name@gmail.com')
+    school_email = models.CharField(max_length=50, default='school_name@gmail.com')
     password = models.CharField(max_length=255, default='passwd')
     school_name = models.CharField(max_length=40)
     school = models.ForeignKey(
